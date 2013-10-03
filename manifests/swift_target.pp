337100395b65b7df3417fb21185e98bbdf1f585d
Rahul Krishna Upadhyaya <rahuupad@cisco.com>
2013-10-03 21:02:43 +0530

    Bug 1206572
    Adding Nagios check for Swift.
    Added new python script check_swift and modified swift_target to
    call it.

--- manifests/swift_target.pp
+++ manifests/swift_target.pp
@@ -3,7 +3,28 @@
 #
 class naginator::swift_target {
 
-    class { "naginator::common_target":
+    class { 'naginator::common_target':
     }
 
+    @@nagios_service { "check_swift_${hostname}":
+      check_command       => 'check_nrpe_1arg!check_swift',
+      use                 => 'generic-service',
+      host_name           => $fqdn,
+      service_description => 'Check_Swift',
+    }
+
+    naginator::nrpe::command { 'check_swift':
+      command => "check_swift -a=http://$::controller_node_address:5000/v2.0/ \
+      -u=admin -k=$admin_password -x=admin";
+    }
+
+    file { 'check_swift':
+      name    => '/usr/lib/nagios/plugins/check_swift',
+      source  => 'puppet:///modules/naginator/check_swift',
+      mode    => '0755',
+      owner   => 'root',
+      group   => 'root',
+      require => Package['nagios-plugins'],
+      notify  => Service['nagios-nrpe-server'],
+    }
 }
