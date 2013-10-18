1c91ae2866876bb35849a0d2b120eaf6b032a356
Rahul Krishna Upadhyaya <rahuupad@cisco.com>
2013-10-18 13:57:28 +0530

    BUG 1240096 Corrected nagios compute check
    Compute_target.pp : Corrected inheritance so redefine doesnt happen
    and fixed lint errors and warnings.
    
    Common_taget.pp : fixed lint errors and warnings.

--- manifests/compute_target.pp
+++ manifests/compute_target.pp
@@ -1,32 +1,29 @@
-# 
+#
 # class to define compute-node specific monitoring
 #
 
-class naginator::compute_target {
+class naginator::compute_target inherits naginator::common_target{
 
     include naginator::params
-
-    class { "naginator::common_target":
-    }
 
     #
     # custom plugin to monitor resource usage due to VMs on compute nodes
 
-    @@nagios_service { "check_vm_${hostname}":
-        check_command       => "check_nrpe_1arg!check_vm",
-        use                 => "generic-service",
-        host_name           => "$fqdn",
-        service_description => "VM Stats",
+    @@nagios_service { "check_vm_${::hostname}":
+        check_command       => 'check_nrpe_1arg!check_vm',
+        use                 => 'generic-service',
+        host_name           => $::fqdn,
+        service_description => 'VM Stats',
     }
 
-    naginator::nrpe::command { "check_vm":
-        command => "check_vm";
+    naginator::nrpe::command { 'check_vm':
+        command => 'check_vm';
     }
 
-    file { "check_vm":
+    file { 'check_vm':
         name    => "${::naginator::params::plugin_dir}/check_vm",
         source  => 'puppet:///modules/naginator/check_vm',
-        mode    => 0755,
+        mode    => '0755',
         owner   => root,
         group   => root,
         require => Package[$::naginator::params::nagios_plugin],
